17d4d93fd9d4ba6261daea6a5fcccc15da7d0420
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-04-27 14:35:17 +0100

    Whooo progress!

---
+++ TerraLib/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TerraLib;
+
+namespace TerraLib
+{
+    public class Class1
+    {
+        public void initterralib()
+        {
+            Console.WriteLine("This doesn't do anything!\nAt All!");
+            Console.ReadLine();
+
+        }
+
+        public void initff9terramodloader()
+        {
+            loader init = new loader();
+            init.modloader();
+            //Console.WriteLine("Searching mods folder...");
+        }
+    }
+}
